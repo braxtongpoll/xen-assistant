406456ca1633ed70a66ecb72104cff430dbefb89
PlutoTheDev <55970625+braxtongpoll@users.noreply.github.com>
2021-09-07 16:38:28 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,10 @@
 const config = {
-    token: "NzE2ODI2ODA3NDI4NzEwNDEx.XtRa_A.sfUq01zJWT0D3hjYzTiwvpeW-nQ",
-    database: "mongodb+srv://bgpoll:900089011@cluster.rphbc.mongodb.net/xen",
-    helpEmbedWelcome: "Welcome to Xen Assistant! I am here to help you around Xen Development. Scroll below to see all my commands that you have access to.\n\nFor product support see: <#716169686416490538>\nFor support for one of our bots see: <#716169666875359253>",
+    token: "",
+    database: "",
+    helpEmbedWelcome: "Welcome to Xen Assistant! I am here to help you around. Scroll below to see all my commands that you have access to.",
     color: "00ccff",
     devs: [
-        //Braxton
-        `399718367335940117`,
-        //Jordan
-        `389558396195438593`
+    ""
     ]
 };
 
